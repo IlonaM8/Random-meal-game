b06caf9b82fcaf67df90fc8af8bfe99c9e84d8a3
IlonaM8 <edoilo.3521@gmail.com>
2021-10-03 21:46:50 +0200

    Random meal generator first steps

---
+++ script.js
@@ -0,0 +1,8 @@
+const get_meal_btn = document.getElementById('get_meal');
+const meal_container = document.getElementById('meal');
+
+
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello there!';
+
+
