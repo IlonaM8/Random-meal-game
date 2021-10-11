7549525198a45493bef5dcb3e9322fd41983b8dd
IlonaM8 <edoilo.3521@gmail.com>
2021-10-11 09:17:38 +0200

    random item from an array

--- script.js
+++ script.js
@@ -1,8 +1,64 @@
 const get_meal_btn = document.getElementById('get_meal');
 const meal_container = document.getElementById('meal');
 
-
+/*
+//change h1 when you click :))
 const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello there!';
+myHeading.onclick = function() {
+    myHeading.textContent = 'Hello there!'; 
+};
+*/
+
+
+
+let  myFood = ["Pizza", "Pasta", "Tofu", "Chickpeas", "Lentil Soup"];
+ 
+function randomItem (myFood) {
+    return myFood[Math.floor(Math.random()*myFood.length)];
+};
+
+console.log(randomItem(myFood));
+
+
+  
+
+// stop poking the button
+/*document.querySelector('button').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+*/
+
+
+//personalized welcome message change it from using a button to ...?
+/*
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name');
+    if(!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+    myHeading.textContent = 'Hi' + ' ' + myName + '! ' + 'Are you hungry?';
+
+    };
+};
+
+
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Hi' + ' ' + storedName +  '! ' +'Are you hungry?';
+};
+
+myButton.onclick = function() {
+    setUserName();
+};
+*/
+
 
 
